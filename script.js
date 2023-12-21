d4f000425fe4888a396be734b65bb83f09e14f53
Sacred97 <36605449+Sacred97@users.noreply.github.com>
2023-12-21 16:22:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -55,6 +55,140 @@
 // });
 
 
+let texts = ['Пополнение рекламного бюджета', 'который помогает с маркировкой', 'который добавляет бонусы', 'А что еще?'];
+let typingSpeed = 10; // скорость печатания (миллисекунды)
+let deletingSpeed = 20; // скорость удаления (миллисекунды)
+let pauseTime = 2000; // пауза перед удалением (миллисекунды)
+let container = document.getElementById('writer');
+let textIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+function type() {
+    if (isDeleting) {
+        // Удаляем символы
+        charIndex--;
+    } else {
+        // Добавляем символы
+        charIndex++;
+    }
+
+    // Показываем текущее состояние текста
+    let textToShow = texts[textIndex].substring(0, charIndex);
+    container.textContent = textToShow;
+
+    // Определяем следующий шаг
+    if (!isDeleting && charIndex === texts[textIndex].length) {
+        if (textIndex === texts.length - 1) return;
+        // Если текст полностью напечатан, начинаем удалять
+        isDeleting = true;
+        setTimeout(type, pauseTime); // Пауза перед удалением
+    } else if (isDeleting && charIndex === 0) {
+        // Если текст полностью удален
+        isDeleting = false;
+        textIndex++; // Переходим к следующему тексту
+        if (textIndex === texts.length) {
+            // Если тексты закончились, не повторяем
+            return;
+        }
+        setTimeout(type, typingSpeed); // Пауза перед новым текстом
+    } else {
+        // Повторяем печать/удаление
+        let delay = isDeleting ? deletingSpeed : typingSpeed;
+        setTimeout(type, delay);
+    }
+}
+
+let $promo = document.querySelector('.promo-container')
+let $hiddenHeader = document.querySelector('.hidden-header')
+let $progressBar = document.getElementById('progress-bar')
+
+let $kotbot_how = document.getElementById('how')
+let howPlayed = false
+let $kotbot_bonus = document.getElementById('bonus')
+let bonusPlayed = false
+let $kotbot_origin = document.getElementById('origin')
+let originPlayed = false
+
+let $kotbot_skills = document.getElementById('skills')
+let skillsPlayed = false
+let $kotbot_referral = document.getElementById('referral')
+let referralPlayed = false
+let $kotbot_tour = document.getElementById('tour')
+let tourPlayed = false
+
+let $kotbot_description = document.querySelector('.skills-container')
+let description_animationPlayed = false
+
+function checkAnimation(element, style, value, func, ms = 1000) {
+    const rect = element.getBoundingClientRect()
+    if (rect.top < window.innerHeight && rect.bottom >= 0) {
+        element.style[style] = value
+        if (func) setTimeout(func, ms)
+        return true
+    }
+    return false
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    let skillsRect = $kotbot_skills.getBoundingClientRect()
+    if (skillsRect.top < window.innerHeight && skillsRect.bottom >= 0) {
+        skillsPlayed = true
+        setTimeout(type, 1000)
+    } else {
+        $kotbot_skills.style.transition = 'transform 0.5s';
+        $kotbot_skills.style.transform = 'scale(0.7)';
+    }
+
+    let referral = $kotbot_referral.getBoundingClientRect()
+    if (referral.top < window.innerHeight && referral.bottom >= 0) {
+        referralPlayed = true
+    } else {
+        $kotbot_referral.style.transition = 'transform 0.5s';
+        $kotbot_referral.style.transform = 'scale(0.7)';
+    }
 
 
+    window.addEventListener('scroll', function() {
+        let height = $promo.offsetHeight;
 
+        if (window.pageYOffset > height) {
+            $hiddenHeader.style.top = '0px';
+        } else {
+            $hiddenHeader.style.top = '-90px';
+        }
+
+        let scrollTotal = document.documentElement.scrollHeight - window.innerHeight;
+        let scrollProgress = window.pageYOffset / scrollTotal * 100;
+        $progressBar.style.width = scrollProgress + "%";
+
+
+        if (!howPlayed) {
+            let el = document.querySelector('.how-it-works__title')
+            const rect = el.getBoundingClientRect()
+            if (rect.top < window.innerHeight && rect.bottom >= 0) {
+                el.classList.add('animate__animated', 'animate__fadeInDown')
+                howPlayed = true
+            }
+            howPlayed = false
+        }
+        if (!skillsPlayed) skillsPlayed = checkAnimation($kotbot_skills,'transform', 'scale(1)', type)
+        if (!referralPlayed) referralPlayed = checkAnimation($kotbot_referral)
+    });
+
+});
+
+function getPromo() {
+    let $el = document.getElementById('code')
+    if ($el.innerHTML === 'Погладь кота') {
+        $el.innerHTML = 'Отлично! Дарю промокод GIVE500 -<br/>бонус на первое пополнение'
+        // $el.style.top = "-126px"
+        // $el.style.opacity = "1"
+        $el.classList.remove('hide')
+        return
+    }
+    $el.innerHTML = 'Погладь кота'
+    // $el.style.top = "-101px"
+    // $el.style.opacity = "0.3"
+    $el.classList.add('hide')
+}
